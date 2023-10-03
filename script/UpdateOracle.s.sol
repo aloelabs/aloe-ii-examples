7c553662f4b498beaa17da7e98c40f53f1c1b52a
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-10-03 00:47:32 -0500

    Get UpdateOracle script working

--- script/UpdateOracle.s.sol
+++ script/UpdateOracle.s.sol
@@ -9,18 +9,18 @@ contract UpdateOracleScript is KeeperScript {
     function setUp() public {}
 
     function run() external {
-        vm.createSelectFork(vm.rpcUrl("optimism"));
-        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        OracleUpdateHelper(0xB93d750Cc6CA3d1F494DC25e7375860feef74870).update(poolsOptimism);
-        vm.stopBroadcast();
+        // vm.createSelectFork(vm.rpcUrl("optimism"));
+        // vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        // OracleUpdateHelper(0xB93d750Cc6CA3d1F494DC25e7375860feef74870).update(poolsOptimism);
+        // vm.stopBroadcast();
 
         vm.createSelectFork(vm.rpcUrl("arbitrum"));
-        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY_UPDATE_ORACLE"));
         OracleUpdateHelper(0xDF47F3C81898D9cC3e61979ADD2756ef44893fFa).update(poolsArbitrum);
         vm.stopBroadcast();
 
         vm.createSelectFork(vm.rpcUrl("base"));
-        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY_UPDATE_ORACLE"));
         OracleUpdateHelper(0xDF47F3C81898D9cC3e61979ADD2756ef44893fFa).update(poolsBase);
         vm.stopBroadcast();
     }
