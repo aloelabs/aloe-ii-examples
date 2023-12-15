a61faada621e2195caa418e439d28cdcf5c280d3
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-12-15 00:08:51 -0600

    Update OracleHelper for new deployment

--- script/UpdateOracle.s.sol
+++ script/UpdateOracle.s.sol
@@ -9,19 +9,24 @@ contract UpdateOracleScript is KeeperScript {
     function setUp() public {}
 
     function run() external {
-        // vm.createSelectFork(vm.rpcUrl("optimism"));
-        // vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        // OracleUpdateHelper(0xB93d750Cc6CA3d1F494DC25e7375860feef74870).update(poolsOptimism);
-        // vm.stopBroadcast();
+        vm.createSelectFork(vm.rpcUrl("mainnet"));
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY_UPDATE_ORACLE"));
+        OracleUpdateHelper(0x0F574c68dDa1181DE99eBe86E1639A9E33354077).update(poolsMainnet);
+        vm.stopBroadcast();
+
+        vm.createSelectFork(vm.rpcUrl("optimism"));
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY_UPDATE_ORACLE"));
+        OracleUpdateHelper(0x0F574c68dDa1181DE99eBe86E1639A9E33354077).update(poolsOptimism);
+        vm.stopBroadcast();
 
         vm.createSelectFork(vm.rpcUrl("arbitrum"));
         vm.startBroadcast(vm.envUint("PRIVATE_KEY_UPDATE_ORACLE"));
-        OracleUpdateHelper(0xDF47F3C81898D9cC3e61979ADD2756ef44893fFa).update(poolsArbitrum);
+        OracleUpdateHelper(0x0F574c68dDa1181DE99eBe86E1639A9E33354077).update(poolsArbitrum);
         vm.stopBroadcast();
 
         vm.createSelectFork(vm.rpcUrl("base"));
         vm.startBroadcast(vm.envUint("PRIVATE_KEY_UPDATE_ORACLE"));
-        OracleUpdateHelper(0xDF47F3C81898D9cC3e61979ADD2756ef44893fFa).update(poolsBase);
+        OracleUpdateHelper(0x0F574c68dDa1181DE99eBe86E1639A9E33354077).update(poolsBase);
         vm.stopBroadcast();
     }
 }
