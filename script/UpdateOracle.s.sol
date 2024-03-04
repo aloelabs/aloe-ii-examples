c409e3b4d52a88dbda437b811c4c5248f3223c0d
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2024-03-03 22:42:00 -0600

    Change UpdateOracle script to update at least once a day

--- script/UpdateOracle.s.sol
+++ script/UpdateOracle.s.sol
@@ -21,16 +21,19 @@ contract UpdateOracleScript is KeeperScript {
         IUniswapV3Pool[] memory poolsToUpdate = new IUniswapV3Pool[](pools.length);
         uint256 j;
         for (uint256 i; i < pools.length; i++) {
-            oracleUpdateHelper.ORACLE().update(pools[i], 1 << 32);
-            (, uint40 time, uint256 oldIV, uint256 newIV) = oracleUpdateHelper.ORACLE().lastWrites(pools[i]);
+            (, uint40 oldTime, , ) = oracleUpdateHelper.ORACLE().lastWrites(pools[i]);
 
-            if (time != block.timestamp) continue;
+            if (block.timestamp - oldTime < 1 days) {
+                oracleUpdateHelper.ORACLE().update(pools[i], 1 << 32);
+                (, uint40 time, uint256 oldIV, uint256 newIV) = oracleUpdateHelper.ORACLE().lastWrites(pools[i]);
 
-            if (FixedPointMathLib.abs(int256(newIV) - int256(oldIV)) > updateThreshold) {
-                poolsToUpdate[j] = pools[i];
-                j++;
-                console2.log(address(pools[i]));
+                if (time != block.timestamp) continue;
+                if (FixedPointMathLib.abs(int256(newIV) - int256(oldIV)) < updateThreshold) continue;
             }
+
+            poolsToUpdate[j] = pools[i];
+            j++;
+            console2.log(address(pools[i]));
         }
 
         assembly ("memory-safe") {
