b0d8944e1e0da591960f7a0388b6f42520faa85e
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2024-06-25 17:46:00 -0500

    Create BadDebtProcessor contract and layout usage in test file

---
+++ src/BadDebtProcessor.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20, SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {IUniswapV3FlashCallback} from "v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol";
+import {IUniswapV3SwapCallback} from "v3-core/contracts/interfaces/callback/IUniswapV3SwapCallback.sol";
+import {IUniswapV3Pool} from "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+import {LIQUIDATION_GRACE_PERIOD} from "aloe-ii-core/libraries/constants/Constants.sol";
+import {BalanceSheet, AuctionAmounts, Assets, Prices} from "aloe-ii-core/libraries/BalanceSheet.sol";
+import {TickMath} from "aloe-ii-core/libraries/TickMath.sol";
+
+import {Borrower, ILiquidator} from "aloe-ii-core/Borrower.sol";
+import {Lender} from "aloe-ii-core/Lender.sol";
+
+contract BadDebtProcessor is ILiquidator, IUniswapV3FlashCallback, IUniswapV3SwapCallback {
+    using SafeTransferLib for ERC20;
+
+    receive() external payable {}
+
+    function process(Lender lender, Borrower borrower, IUniswapV3Pool flashPool, uint256 slippage) external {
+        ERC20 asset = lender.asset();
+
+        uint256 withdrawAmount = lender.underlyingBalance(msg.sender);
+        // We assume `withdrawAmount > lender.lastBalance()`, otherwise there's no reason to use this fn
+        uint256 repayAmount = (withdrawAmount - lender.lastBalance()) * 100_010 / 100_000;
+        uint256 closeFactor = ((repayAmount * 10_000) / lender.borrowBalance(address(borrower)));
+
+        bytes memory data = abi.encode(borrower, closeFactor, msg.sender, repayAmount, slippage);
+        if (address(asset) == flashPool.token0()) {
+            flashPool.flash(address(this), repayAmount, 0, data);
+        } else {
+            flashPool.flash(address(this), 0, repayAmount, data);
+        }
+    }
+
+    function uniswapV3FlashCallback(uint256 fee0, uint256 fee1, bytes calldata data) external {
+        require(fee0 == 0 || fee1 == 0, "Incompatible situation");
+
+        (Borrower borrower, uint256 closeFactor, address withdrawFrom, uint256 flashAmount, uint256 slippage) = abi.decode(data, (Borrower, uint256, address, uint256, uint256));
+        borrower.liquidate(this, data, closeFactor, 1 << 32);
+
+        ERC20 flashToken;
+        if (fee0 > 0) {
+            flashToken = ERC20(IUniswapV3Pool(msg.sender).token0());
+            flashAmount += fee0;
+        } else {
+            flashToken = ERC20(IUniswapV3Pool(msg.sender).token1());
+            flashAmount += fee1;
+        }
+
+        ERC20 token0 = borrower.TOKEN0();
+        ERC20 token1 = borrower.TOKEN1();
+        if (token0 == flashToken) {
+            borrower.LENDER0().redeem(type(uint256).max, address(this), withdrawFrom);
+            int256 exactAmountOut = int256(flashToken.balanceOf(address(this))) - int256(flashAmount);
+
+            uint256 recovered = token1.balanceOf(address(this));
+            int256 swapped;
+            if (exactAmountOut < 0) {
+                // Need to swap `token1` (the one we received in during liquidation) for `token0` (which is `flashToken`)
+                (, swapped) = borrower.UNISWAP_POOL().swap(address(this), false, exactAmountOut, TickMath.MAX_SQRT_RATIO - 1, bytes(""));
+                require(uint256(swapped) < recovered * slippage / 10_000, "slippage");
+            }
+
+            token1.safeTransfer(withdrawFrom, recovered - uint256(swapped));
+        } else {
+            borrower.LENDER1().redeem(type(uint256).max, address(this), withdrawFrom);
+            int256 exactAmountOut = int256(flashToken.balanceOf(address(this))) - int256(flashAmount);
+
+            uint256 recovered = token0.balanceOf(address(this));
+            int256 swapped;
+            if (exactAmountOut < 0) {
+                // Need to swap `token0` (the one we received in during liquidation) for `token1` (which is `flashToken`)
+                (swapped, ) = borrower.UNISWAP_POOL().swap(address(this), true, exactAmountOut, TickMath.MIN_SQRT_RATIO + 1, bytes(""));
+                require(uint256(swapped) < recovered * slippage / 10_000, "slippage");
+            }
+
+            token0.safeTransfer(withdrawFrom, recovered - uint256(swapped));
+        }
+
+        flashToken.safeTransfer(msg.sender, flashAmount);
+        flashToken.safeTransfer(withdrawFrom, flashToken.balanceOf(address(this)));
+        if (closeFactor == 10000) payable(withdrawFrom).transfer(address(this).balance);
+    }
+
+    function uniswapV3SwapCallback(int256 amount0Delta, int256 amount1Delta, bytes calldata) external {
+        if (amount0Delta > 0) {
+            ERC20(IUniswapV3Pool(msg.sender).token0()).safeTransfer(msg.sender, uint256(amount0Delta));
+        } else {
+            ERC20(IUniswapV3Pool(msg.sender).token1()).safeTransfer(msg.sender, uint256(amount1Delta));
+        }
+    }
+
+    /**
+     * @notice Transfers `amounts.out0` and `amounts.out1` to the liquidator with the expectation that they'll
+     * transfer `amounts.repay0` and `amounts.repay1` to the appropriate `Lender`s, executing swaps if necessary.
+     * The liquidator can keep leftover funds as a reward.
+     * @param amounts The key amounts involved in the liquidation
+     */
+    function callback(bytes calldata, address, AuctionAmounts memory amounts) external {
+        if (amounts.repay0 > 0) {
+            ERC20 token0 = Borrower(payable(msg.sender)).TOKEN0();
+            Lender lender0 = Borrower(payable(msg.sender)).LENDER0();
+            token0.safeTransfer(address(lender0), amounts.repay0);
+        }
+        if (amounts.repay1 > 0) {
+            ERC20 token1 = Borrower(payable(msg.sender)).TOKEN1();
+            Lender lender1 = Borrower(payable(msg.sender)).LENDER1();
+            token1.safeTransfer(address(lender1), amounts.repay1);
+        }
+    }
+}
---
+++ test/BadDebtProcessor.t.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+import {FixedPointMathLib as SoladyMath} from "solady/utils/FixedPointMathLib.sol";
+
+import {BadDebtProcessor, IUniswapV3Pool, Borrower, Lender, ERC20, Prices} from "src/BadDebtProcessor.sol";
+
+interface ISuperRegistry {
+    function getAddress(bytes32 hash) external view returns (address);
+}
+
+interface IERC4626Form {
+    function vault() external view returns (Lender);
+
+    function superRegistry() external view returns (ISuperRegistry);
+
+    function emergencyWithdraw(address to, uint256 amount) external;
+}
+
+contract ExampleDistributor {
+    IERC4626Form public immutable FORM;
+
+    BadDebtProcessor public immutable PROCESSOR;
+
+    ERC20 public token0;
+
+    ERC20 public token1;
+
+    constructor(IERC4626Form form) {
+        FORM = form;
+        PROCESSOR = new BadDebtProcessor();
+    }
+
+    // TODO: Merkle-based (or other method of) pro-rata distribution of both tokens to FORM users
+
+    function emergencyWithdraw(ERC20 token, address to, uint256 amount) external {
+        require(msg.sender == FORM.superRegistry().getAddress(keccak256("EMERGENCY_QUEUE")), "Only EMERGENCY_QUEUE");
+        token.transfer(to, amount);
+    }
+
+    function processBadDebt(Borrower borrower, IUniswapV3Pool flashPool, uint256 minReceived0, uint256 minReceived1) external {
+        require(msg.sender == FORM.superRegistry().getAddress(keccak256("EMERGENCY_QUEUE")), "Only EMERGENCY_QUEUE");
+
+        Lender lender = FORM.vault();
+        lender.approve(address(PROCESSOR), type(uint256).max);
+        PROCESSOR.process(lender, borrower, flashPool, 10); // max loss of 0.1% to flash swap fees
+
+        token0 = borrower.TOKEN0();
+        require(token0.balanceOf(address(this)) > minReceived0, "expected more token0");
+        token1 = borrower.TOKEN1();
+        require(token1.balanceOf(address(this)) > minReceived1, "expected more token1");
+    }
+}
+
+contract BadDebtProcessorTest is Test {
+    BadDebtProcessor processor;
+
+    function setUp() public {
+        processor = new BadDebtProcessor();
+
+        vm.createSelectFork(vm.rpcUrl("base"));
+        vm.rollFork(16280514);
+    }
+
+    function testDistributor() public {
+        // WETH+ Form -- the one impacted by bad debt, and from which users won't be able to withdraw normally
+        IERC4626Form form = IERC4626Form(0xa5254fF645494d93635360c1cacC375191023a8A);
+        // The borrower whose debt can only be liquidated lossily
+        Borrower borrower = Borrower(payable(0xC7Cdda63Bf761c663FD7058739be847b422aA5A2));
+        // The contract that will process the bad debt and distribute the borrower's collateral to users.
+        // When used, this essentially accepts the loss from bad debt, receiving any remaining collateral
+        // even though that collateral isn't worth as much as the nominal deposits.
+        // In this case (at current prices), users would get back ~45 cents on the dollar.
+        ExampleDistributor distributor = new ExampleDistributor(form);
+
+        uint256 nominalEthBalance = form.vault().underlyingBalance(address(form));
+        console2.log("WETH balance (nominal):", nominalEthBalance);
+        console2.log("WETH balance (redeemable):", form.vault().maxWithdraw(address(form)));
+
+        vm.startPrank(form.superRegistry().getAddress(keccak256("EMERGENCY_QUEUE")));
+        // Move WETH+ shares from the form to the distributor
+        form.emergencyWithdraw(address(distributor), form.vault().balanceOf(address(form)));
+        // Redeem shares for the unhealthy borrower's collateral (users can then decide to sell immediately for ETH, or hold and hope value goes back up)
+        // Revert if we don't get at least 76,000 BASED tokens.
+        distributor.processBadDebt(borrower, IUniswapV3Pool(0x20E068D76f9E90b90604500B84c7e19dCB923e7e), 8.5 ether, 76_000e18);
+
+        uint256 ethReceived = distributor.token0().balanceOf(address(distributor));
+        uint256 basedReceived = distributor.token1().balanceOf(address(distributor));
+        console2.log("WETH received:", ethReceived);
+        console2.log("BASED received:", basedReceived);
+
+        (Prices memory prices, , ,) = borrower.getPrices(1 << 32);
+        uint256 priceX128 = SoladyMath.fullMulDiv(prices.c, prices.c, 1 << 64);
+        uint256 basedReceivedInTermsOfEth = SoladyMath.fullMulDiv(basedReceived, 1 << 128, priceX128);
+        console2.log("Recovery fraction at current price:", (ethReceived + basedReceivedInTermsOfEth) * 10_000 / nominalEthBalance, "/ 10000");
+    }
+
+    function testBadDebtProcessor() public {
+        IERC4626Form form = IERC4626Form(
+            0xa5254fF645494d93635360c1cacC375191023a8A
+        );
+        ISuperRegistry superRegistry = form.superRegistry();
+        address emergencyAddress = superRegistry.getAddress(
+            keccak256("EMERGENCY_QUEUE")
+        );
+
+        console2.log(emergencyAddress);
+
+        Lender lender = form.vault();
+        uint256 shares = lender.balanceOf(address(form));
+
+        address receiver = address(12345);
+
+        vm.prank(emergencyAddress);
+        form.emergencyWithdraw(receiver, shares);
+
+        Borrower borrower = Borrower(payable(0xC7Cdda63Bf761c663FD7058739be847b422aA5A2));
+
+        vm.startPrank(receiver);
+        lender.approve(address(processor), type(uint256).max); // NOTE: Dangerous outside of test-env, would want this to be atomic with debt processing
+        processor.process(
+            lender,
+            borrower,
+            IUniswapV3Pool(0x20E068D76f9E90b90604500B84c7e19dCB923e7e),
+            10 // 0.1% slippage
+        );
+        vm.stopPrank();
+
+        console2.log("receiver shares (t0)", shares);
+        console2.log("receiver shares (t1)", lender.balanceOf(receiver));
+        console2.log("receiver token0 (t1)", borrower.TOKEN0().balanceOf(receiver));
+        console2.log("receiver token1 (t1)", borrower.TOKEN1().balanceOf(receiver));
+        console2.log("processor token0 (t1)", borrower.TOKEN0().balanceOf(address(processor)));
+        console2.log("processor token1 (t1)", borrower.TOKEN1().balanceOf(address(processor)));
+        console2.log("lender assets (t1)", lender.asset().balanceOf(address(lender)));
+
+        (uint256 a, uint256 b) = borrower.getLiabilities();
+        console2.log(a, b);
+    }
+}
